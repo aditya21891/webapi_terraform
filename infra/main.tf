5a19ac6ef2595fa0ee8b065424228aed512ab56a
aditya21891 <prattiaditya@gmail.com>
2021-07-26 15:50:58 -0400

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -177,14 +177,7 @@ resource "aws_security_group" "instance_security_group" {
     to_port     = "443"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-   
-  ingress {
-    from_port   = "5000"
-    to_port     = "5000"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  }    
 
   ingress {
     from_port   = "80"
