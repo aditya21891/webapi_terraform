12bcd86a8200fda03312bb18aaa0d5ed3e5b9390
aditya21891 <prattiaditya@gmail.com>
2021-07-26 14:03:14 -0400

    Update userdata.sh

--- infra/userdata.sh
+++ infra/userdata.sh
@@ -8,9 +8,9 @@ pip3 install boto3 --user
 yum remove docker docker-common docker-selinux docker-engine-selinux docker-engine docker-ce -y
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce -y
-systemctl enable docker.service
-systemctl start docker
+yum install docker -y	
+yum install systemd -y	
+service docker start
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
@@ -18,4 +18,5 @@ wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
 ./install auto
 service codedeploy-agent start
+echo "End of UserData"
 echo "End of UserData"
